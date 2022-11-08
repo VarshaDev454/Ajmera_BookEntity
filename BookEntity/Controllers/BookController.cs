0a8977bad8a0f056401e6a7639bd323a4ac0b720
VarshaDev454 <varsha.dev454@gmail.com>
2022-11-09 00:58:20 +0530

    initial commit

---
+++ BookEntity/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿//using Assessment.Logging.Interface;
+//using Assessment.ViewModel;
+//using AutoMapper;
+//using BusinessAccessLayer.Interface;
+//using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Assessment.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        public BookController()
+        {
+            
+        }
+
+        [HttpGet("GetBooks")]
+        public ActionResult<string> GetBooks()
+        {
+            return "Book";
+        }        
+    }
+}
